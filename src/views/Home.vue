66874e101fb771f4fda99afe2d7abc84d2a25c29
Robert Reilley <rpreilley@gmail.com>
2019-10-20 08:56:01 -0500

    add lodash library, set debounce function and change search to fire on input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,45 @@
 <template>
-  <div class="home">
+  <div v-if="loading">
+    Loading...
+  </div>
+  <div v-else class="home">
     <h1>
       Gif Picker
     </h1>
     <div>
-      <p>Random Gifs</p>
+      <!-- <p>Random Gifs</p>
       <div v-for="(gif, index) in randomGifs" :key="index">
         <video autoplay loop>
-          <source :src="gif.images.original_mp4.mp4" alt="Giphy Image" type="video/mp4">
+          <source
+            :src="gif.images.original_mp4.mp4"
+            alt="Giphy Image" type="video/mp4"
+            id="random-text"
+          >
         </video>
         <div>
-          <input type="hidden" id="search-text" :value="gif.images.original_mp4.mp4">
           <button type="button" @click="copyGiphyLink">Copy Link</button>
         </div>
-      </div>
+      </div> -->
       <p>Searched Gifs</p>
-      <form @submit.prevent="searchGiffy">
-        <input v-model="searchText" class="uk-input" type="text">
-        <button type="submit" @click.prevent="searchGiffy">Search</button>
-      </form>
+      <!-- <form @submit.prevent="searchGiffy">
+        <input v-model="searchText" class="uk-input" type="text" @input="searchGiffy">
+      </form> -->
+      <input v-model="searchText" class="uk-input" type="text" @input="debouncedGiphySearch()">
     </div>
     <div v-for="(gif, index) in searchedGifs" :key="index">
       <video autoplay loop>
         <source :src="gif.images.original_mp4.mp4" alt="Giphy Image" type="video/mp4">
       </video>
       <div>
-        <input type="hidden" id="search-text" :value="gif.images.original_mp4.mp4">
-        <button type="button" @click="copyGiphyLink">Copy Link</button>
+        <input type="hidden" :id="`search-text${index}`" :value="gif.images.original_mp4.mp4">
+        <button type="button" @click="copyGiphyLink(index)">Copy Link</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
 import CONSTANTS from '../constants/apiKeys';
 import ENDPOINTS from '../constants/endpoints';
 import ApiService from '../services/ApiService';
@@ -52,10 +59,12 @@ export default {
 
   },
   mounted() {
-    this.getRandomGifs();
+    // this.getRandomGifs();
   },
   methods: {
-    async searchGiffy() {
+    // Debounce function so it isn't called immediately after every keypress
+    debouncedGiphySearch: _.debounce(function () { this.searchGiphy(); }, 750),
+    async searchGiphy() {
       // Clear searchedGifs array if there are any before searching again
       if (this.searchedGifs.length > 0) {
         this.searchedGifs = [];
@@ -66,7 +75,7 @@ export default {
       // Construct query and encode query parameters in the process
       const query = `${url}?q=${encodeURIComponent(this.searchText)}&api_key=${this.key}&limit=20`;
 
-      await ApiService.get(query)
+      return ApiService.get(query)
         .then((response) => {
           if (response && response.status === 200) {
             // Iterate through array and remove any objects that don't contain an mp4 file
@@ -114,8 +123,8 @@ export default {
           console.log(error);
         });
     },
-    copyGiphyLink() {
-      const linkToCopy = document.querySelector('#search-text');
+    copyGiphyLink(id) {
+      const linkToCopy = document.querySelector(`#search-text${id}`);
       linkToCopy.setAttribute('type', 'text');
       linkToCopy.select();
 
@@ -123,6 +132,7 @@ export default {
         const successful = document.execCommand('copy');
         const msg = successful ? 'successful' : 'unsuccessful';
         console.log(`Testing code was copied ${msg}`);
+        // Notify user giphy link has been copied
       } catch (err) {
         console.log('Oops, unable to copy');
       }
